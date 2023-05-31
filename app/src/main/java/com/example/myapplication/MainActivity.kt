39f093150c060cfe518701c79fb46ec9896fd351
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-05-31 16:17:16 +0700

    add normalization

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,15 +20,12 @@ class MainActivity : AppCompatActivity() {
         val button : Button = findViewById(R.id.button)
         val textView : TextView = findViewById(R.id.textView)
 
-
         val assetManager: AssetManager = assets
         val interpreter: Interpreter = loadModel(assetManager)
-
         // Dummy input, not normalized yet. Click button to see the prediction
-        val features = floatArrayOf(2.0f, 3.0f, 1.5f, 4.0f, 2.5f)
-        val result = performInference(interpreter, features)
+        val features = featureNormalization(83460F, 22380F, 92F, 23132.5F, 168.9F) // Quality = 77
+        val result = targetNormalizationInverse(performInference(interpreter, features))
         button.setOnClickListener{
-
             textView.text = result.toString()
         }
     }
@@ -40,14 +37,12 @@ class MainActivity : AppCompatActivity() {
         val fileChannel: FileChannel = inputStream.channel
         val startOffset: Long = modelFileDescriptor.startOffset
         val declaredLength: Long = modelFileDescriptor.declaredLength
-
         val modelByteBuffer: MappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
-
         return Interpreter(modelByteBuffer)
     }
 
     // Perform inference using the TensorFlow Lite model
-    fun performInference(interpreter: Interpreter, features: FloatArray): Float {
+    private fun performInference(interpreter: Interpreter, features: FloatArray): Float {
         val inputShape = interpreter.getInputTensor(0).shape()
         val outputShape = interpreter.getOutputTensor(0).shape()
 
@@ -61,7 +56,23 @@ class MainActivity : AppCompatActivity() {
         outputBuffer.order(ByteOrder.nativeOrder())
 
         interpreter.run(inputBuffer, outputBuffer)
-
         return outputBuffer.getFloat(0)
+    }
+    // normalization func
+    private fun featureNormalization(start:Float,
+                                     end:Float,
+                                     regularity:Float,
+                                     timeAsleep:Float,
+                                     timeBeforeSleep:Float) : FloatArray {
+        val startNorm = (start-21780)/(94380-21780)
+        val endNorm = (end-3900)/(84540-3900)
+        val regularityNorm = (regularity+1)/(100+1)
+        val timeAsleepNorm = (timeAsleep/45769.4)
+        val timeBeforeSleepNorm = (timeBeforeSleep-560)/(5677.7-560)
+
+        return floatArrayOf(startNorm, endNorm, regularityNorm, timeAsleepNorm.toFloat(), timeBeforeSleepNorm.toFloat())
+    }
+    private fun targetNormalizationInverse(quality:Float):Float{
+        return quality * (100-7) + 7
     }
 }
